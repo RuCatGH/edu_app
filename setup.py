77acaa55f178789d4afcecdcf17c57e6a9d1b9ba
RuCat <cvlromacvl@gmail.com>
2025-06-04 13:44:14 +0300

    fix error

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="edu_platform",
     version="0.1",
-    packages=find_packages(),
+    package_dir={"": "edu_platform"},
+    packages=find_namespace_packages(include=["edu_platform*"]),
     install_requires=[
         "Django>=4.2.0",
         "psycopg2-binary>=2.9.0",
